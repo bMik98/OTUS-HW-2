d766169f3781ff6f127b817cce30ee9aec4270ba
bMik98 <bmik9898@gmail.com>
2018-08-11 11:36:26 +0300

    rename

--- src/main/java/ru/otus/spring/bookinfo/Application.java
+++ src/main/java/ru/otus/spring/bookinfo/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookinfoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookinfoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
