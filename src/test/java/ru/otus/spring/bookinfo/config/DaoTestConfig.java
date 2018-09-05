5a915a138a9288eb939e72150ecaa1c23e236da7
bMik98 <bmik9898@gmail.com>
2018-09-06 01:38:58 +0300

    dao

--- src/test/java/ru/otus/spring/bookinfo/config/DaoTestConfig.java
+++ src/test/java/ru/otus/spring/bookinfo/config/DaoTestConfig.java
@@ -2,13 +2,9 @@ package ru.otus.spring.bookinfo.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcOperations;
 import ru.otus.spring.bookinfo.dao.AuthorDao;
 import ru.otus.spring.bookinfo.dao.BookDao;
 import ru.otus.spring.bookinfo.dao.GenreDao;
-import ru.otus.spring.bookinfo.dao.jdbc.JdbcAuthorDao;
-import ru.otus.spring.bookinfo.dao.jdbc.JdbcBookDao;
-import ru.otus.spring.bookinfo.dao.jdbc.JdbcGenreDao;
 import ru.otus.spring.bookinfo.dao.jpa.JpaAuthorDao;
 import ru.otus.spring.bookinfo.dao.jpa.JpaBookDao;
 import ru.otus.spring.bookinfo.dao.jpa.JpaGenreDao;
--- src/test/java/ru/otus/spring/bookinfo/dao/BookDaoTest.java
+++ src/test/java/ru/otus/spring/bookinfo/dao/BookDaoTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@JdbcTest
+@DataJpaTest
 @Import(DaoTestConfig.class)
 public class BookDaoTest {
 
