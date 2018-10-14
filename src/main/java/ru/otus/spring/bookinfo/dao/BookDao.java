61a8feda73ee6f923597f5c458cf3c8748e9f62c
bMik98 <bmik9898@gmail.com>
2018-10-14 20:59:39 +0300

    dao refactoring

--- src/main/java/ru/otus/spring/bookinfo/dao/BookDao.java
+++ src/main/java/ru/otus/spring/bookinfo/dao/BookDao.java
@@ -1,6 +1,14 @@
 package ru.otus.spring.bookinfo.dao;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.otus.spring.bookinfo.domain.Book;
 
-public interface BookDao extends BasicEntityDao<Book> {
+import java.util.List;
+
+@Repository
+public interface BookDao extends CrudRepository<Book, Integer> {
+
+    List<Book> findAll();
+
 }
--- src/main/java/ru/otus/spring/bookinfo/dao/GenreDao.java
+++ src/main/java/ru/otus/spring/bookinfo/dao/GenreDao.java
@@ -1,6 +1,14 @@
 package ru.otus.spring.bookinfo.dao;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.otus.spring.bookinfo.domain.Genre;
 
-public interface GenreDao extends BasicEntityDao<Genre> {
+import java.util.List;
+
+@Repository
+public interface GenreDao extends CrudRepository<Genre, Integer> {
+
+    List<Genre> findAll();
+
 }
